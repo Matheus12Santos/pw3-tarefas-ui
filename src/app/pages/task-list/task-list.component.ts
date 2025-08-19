28b4a3f753a07f260b90221804b14026b706dc54
unknown <matheus12santos@etec.sp.gov.br>
2025-08-19 11:04:18 -0300

    Implementando a API

---
+++ src/app/pages/task-list/task-list.component.ts
@@ -0,0 +1,39 @@
+import { TaskService } from './../../service/task.service';
+import { Component, OnInit, inject } from '@angular/core';
+import { Task } from '../../model/task';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-task-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css'],
+})
+export class TaskListComponent implements OnInit {
+
+  tasks: Task[] = [];
+  private router = inject(Router);
+  private taskService = inject(TaskService);
+
+  ngOnInit(): void {
+    this.taskService.findAll().subscribe((tasks) =>{
+      this.tasks = tasks;
+    })
+  }
+
+  newTask() {
+    this.router.navigate(['/tasks/new']);
+  }
+
+  deleteTask(id: number | undefined) {
+    if (id && confirm('Tem certeza que deseja excluir esta tarefa?')) {
+      console.log("Exclusão realizada");
+    }
+  }
+
+  editTask(id: number | undefined) {
+    if (id) this.router.navigate(['/tasks', id]);
+  }
+}
---
+++ src/app/service/task.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../model/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:4200/tarefas';
+  private http = inject(HttpClient);
+
+  findAll(): Observable<Task[]>{
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  constructor() { }
+}
